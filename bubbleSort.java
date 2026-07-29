4ce4c8e73494dd9a4a8ceb554d415427b061b590
Bruno Henrique Kobayassi <brunokobayassi@gmail.com>
2026-07-29 19:57:51 -0300

    teste

---
+++ bubbleSort.java
@@ -0,0 +1,22 @@
+public class bubbleSort {
+    public static void main(String[] args) {
+        int[] vetor = { 10, 5, 8, 2, 7 };
+        int flag = 1;
+
+        while (flag == 1) {
+            flag = 0;
+            for (int i = 0; i < (vetor.length - 1); i++) {
+                if (vetor[i] < vetor[i + 1]) {
+                    int aux = vetor[i];
+                    vetor[i] = vetor[i + 1];
+                    vetor[i + 1] = aux;
+                    flag = 1;
+                }
+            }
+        }
+
+        for (int i : vetor) {
+            System.out.println(i);
+        }
+    }
+}
